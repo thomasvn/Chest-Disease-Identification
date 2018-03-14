945bf1662359b71e39489e33d8d982a951a240b0
thomasvn <thomasnguyen96@gmail.com>
2018-03-13 22:57:32 -0700

    Updated comments, increased training size, shuffled the training data before use

--- convnet.py
+++ convnet.py
@@ -8,77 +8,123 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.datasets import mnist
 
-NUM_IMG = 50
+NUM_IMG = 100
 
 def get_training_data(train_path, labels_path):
-	train_images = []
-	train_files = []  # gather names of files to train
-	for filename in os.listdir(train_path):
-		if filename.endswith(".png"):
-			train_files.append(train_path + filename)
+    train_files = []  # gather names of files to train
+    for filename in os.listdir(train_path):
+        if filename.endswith(".png"):
+            train_files.append(train_path + filename)
 
-	features = []  # load images as arrays
-	for i, train_file in enumerate(train_files):
-		if i >= NUM_IMG: break
-		train_image = imread(train_file, True)  # loads image from file
-		feature_set = np.asarray(train_image)
-		features.append(feature_set)
+    features = []  # load images as arrays
+    for i, train_file in enumerate(train_files):
+        if i >= NUM_IMG: break
+        train_image = imread(train_file, True)  # loads image from file
+        feature_set = np.asarray(train_image)
+        features.append(feature_set)
 
-	labels_df = pd.read_csv(labels_path)  # retrieve labels of images
-	labels_df = labels_df["Finding Labels"]
-	labels = np.zeros(NUM_IMG)  # labels column vector (0 for no finding, 1 for finding)
+    labels_df = pd.read_csv(labels_path)  # retrieve labels of images
+    labels_df = labels_df["Finding Labels"]
+    labels = np.zeros(NUM_IMG)  # labels column vector (0 for no finding, 1 for finding)
 
-	# adjust labels column vector
-	for i in range(NUM_IMG):
-		if (labels_df[i] == 'No Finding'):
-			labels[i] = 0
-		else:
-			labels[i] = 1
-	images = np.expand_dims(np.array(features), axis=3).astype('float32') / 255  # adding single channel
-	return images, labels
+    # adjust labels column vector
+    for i in range(NUM_IMG):
+        if (labels_df[i] == 'No Finding'):
+            labels[i] = 0
+        else:
+            labels[i] = 1
+    images = np.expand_dims(np.array(features), axis=3).astype('float32') / 255  # adding single channel
+    return images, labels
 
 
 if __name__ == "__main__":
-	x_train, y_train = get_training_data("data/train/", "data/train-labels.csv")
-	x_test, y_test = get_training_data("data/test/", "data/test-labels.csv")
+    x_train, y_train = get_training_data("data/train/", "data/train-labels.csv")
+    x_test, y_test = get_training_data("data/test/", "data/test-labels.csv")
 
-	f = open("log.txt","w")
-	f.write("--------- Images ----------\n")
-	f.write(str(x_train) + "\n")
-	f.write("--------- Labels ----------\n")
-	f.write(str(y_train) + "\n")
-	f.close()
+    f = open("log.txt","w")
+    f.write("--------- Images ----------\n")
+    f.write(str(x_train) + "\n")
+    f.write("--------- Labels ----------\n")
+    f.write(str(y_train) + "\n")
+    f.close()
 
-	# linear stack of layers
-	model = Sequential()
+    # linear stack of layers
+    model = Sequential()
 
-	# Layer 1
-	# rectifier linear unit [f(x) = max(0,x)]
-	model.add(Conv2D(4, (3, 3), strides=(2,2), activation='relu', input_shape=(1024, 1024, 1), data_format='channels_last'))
+    # Layer 1
+    model.add(
+        Conv2D(
+            4,  # number of filters (dimensionality of output space)
+            (3, 3),  # kernel size (width and height of 2D convolutional window)
+            strides=(2,2), 
+            activation='relu',  # rectifier linear unit [f(x) = max(0,x)]
+            input_shape=(1024, 1024, 1),  # (samples, rows, cols, channels)
+            data_format='channels_last')
+    )
 
-	# Layer 2
-	# max pooling
-	model.add(MaxPooling2D(pool_size=(2,2)))
+    # Layer 2
+    model.add(
+        MaxPooling2D(
+            pool_size=(2,2)  # downscaling factors (vert, horiz)
+        )
+    )
 
-	# Layer 3
-	model.add(Conv2D(4, (3, 3), strides=(2,2), activation='relu'))
+    # Layer 3
+    model.add(
+        Conv2D(
+            4,  # number of filters
+            (3, 3),  # kernel size
+            strides=(2,2), 
+            activation='relu')
+    )
 
-	# Layer 4
-	# randomly drops 25% of input to prevent overfitting
-	model.add(Dropout(0.25))
+    # Layer 4
+    model.add(
+        Dropout(0.25)  # randomly drops 25% of input to prevent overfitting
+    )
 
-	# Layer 5
-	# flattens the output shape of the input
-	model.add(Flatten())
+    # Layer 5
+    model.add(
+        Flatten()  # flattens the output shape of the input
+    )
 
-	# Layer 6
-	# dense layers will perform classification on features extracted from convolution and maxPooling
-	model.add(Dense(1024, activation='relu'))
+    # Layer 6
+    # perform classification on features extracted from conv and maxPooling
+    model.add(
+        Dense(
+            1024,  # dimensionality of output space
+            activation='relu'
+        )
+    )
 
-	# Layer 7
-	# dimensionality of output space is one
-	model.add(Dense(1, activation='sigmoid'))
+    # Layer 7
+    model.add(
+        Dense(
+            1,  # dimensionality of output space
+            activation='sigmoid'
+        )
+    )
 
-	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) 
-	model.fit(x_train, y_train, batch_size=4, nb_epoch=10, verbose=1)
-	score = model.evaluate(x_test, y_test, verbose=0)
+    # Configuration of learning process
+    model.compile(
+        loss='binary_crossentropy', 
+        optimizer='adam', 
+        metrics=['accuracy']
+    )
+
+    # Iteration on the training data
+    model.fit(
+        x_train,
+        y_train,
+        batch_size=10,  # num of samples propogated through network
+        epochs=10,  # num of forward and backward pass of all train samples
+        shuffle=True,
+        verbose=1
+    )
+
+    # Evaluate performance
+    score = model.evaluate(
+        x_test,
+        y_test,
+        verbose=0
+    )
